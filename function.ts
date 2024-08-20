6be6b02450d94862490ef778cc60f30e338c1361
Sulaim <sulaim@Sulaims-Air.local>
2024-08-20 12:47:18 +0530

    third commit

---
+++ function.ts
@@ -0,0 +1,19 @@
+function  addByTwo(num : number):number{
+  return num +2
+}
+
+function neverReturnFn(msg : string):never{
+  throw new Error('This is the erro')
+}
+
+function voidReturnFn(msg : string):void{
+  return 
+}
+// in the function argument the type of the variable should be specified else it will assigne as the any for the arguemnt and the function return type 
+
+
+const arrowFn = (isPlaced : boolean = true):boolean =>{
+  return true
+}
+
+// above fun is example for arrow fuction and how assign a default value
